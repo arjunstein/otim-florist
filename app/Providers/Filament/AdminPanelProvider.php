646c6cc645b566c7443b8a2289a2db458bae5f3e
arjunstein <arjun.gnw09@gmail.com>
2024-07-25 10:03:26 +0700

    ✨ Added favicon to Filament admin panel configuration.

--- app/Providers/Filament/AdminPanelProvider.php
+++ app/Providers/Filament/AdminPanelProvider.php
@@ -28,6 +28,7 @@ class AdminPanelProvider extends PanelProvider
             ->path('admin')
             ->login()
             ->spa()
+            ->favicon(asset('img/favicon.png'))
             ->colors([
                 'primary' => Color::Blue,
             ])

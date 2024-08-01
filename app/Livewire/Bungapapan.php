2588473119b819cba03e64bd619e9836198f15c6
arjunstein <arjun.gnw09@gmail.com>
2024-08-01 21:56:13 +0700

    ✨ feat: add SEO config in each pages

--- app/Livewire/Bungapapan.php
+++ app/Livewire/Bungapapan.php
@@ -4,24 +4,44 @@ namespace App\Livewire;
 
 use App\Models\Category;
 use App\Models\Product;
-use Livewire\Attributes\Title;
+use Artesaos\SEOTools\Facades\JsonLd;
+use Artesaos\SEOTools\Facades\OpenGraph;
+use Artesaos\SEOTools\Facades\SEOMeta;
+use Artesaos\SEOTools\Facades\TwitterCard;
+use Illuminate\Support\Facades\Cache;
+use Illuminate\Support\Facades\Storage;
+use Livewire\Attributes\Computed;
 use Livewire\Component;
 use Livewire\WithPagination;
-use Illuminate\Support\Facades\Cache;
-use Livewire\Attributes\Computed;
 
 class Bungapapan extends Component
 {
     use WithPagination;
 
     #[Computed(cache: true)]
-    #[Title('Bunga papan - Otim Florist Jakarta')]
+
     public $category;
     public $amount = 10;
     protected $paginationTheme = 'bootstrap';
 
     public function mount()
     {
+        SEOMeta::setTitle('Otim Florist Jakarta');
+        SEOMeta::setDescription('Toko bunga online yang menawarkan berbagai macam bunga segar untuk berbagai acara seperti ulang tahun, pernikahan, dan hari spesial lainnya. Pilih dari berbagai buket dan karangan bunga yang cantik dan menawan');
+        SEOMeta::setCanonical('https://otimflorist.com');
+
+        OpenGraph::setDescription('Toko bunga online yang menawarkan berbagai macam bunga segar untuk berbagai acara seperti ulang tahun, pernikahan, dan hari spesial lainnya. Pilih dari berbagai buket dan karangan bunga yang cantik dan menawan');
+        OpenGraph::setTitle('Otim Florist Jakarta');
+        OpenGraph::setUrl('https://otimflorist.com');
+        OpenGraph::addProperty('type', 'website');
+
+        TwitterCard::setTitle('Otim Florist Jakarta');
+        TwitterCard::setSite('@otimfloristjakarta');
+
+        JsonLd::setTitle('Otim Florist Jakarta');
+        JsonLd::setDescription('Toko bunga online yang menawarkan berbagai macam bunga segar untuk berbagai acara seperti ulang tahun, pernikahan, dan hari spesial lainnya. Pilih dari berbagai buket dan karangan bunga yang cantik dan menawan');
+        JsonLd::setImages(Storage::url('img/favicon.png'));
+
         $this->category = $this->getCategories();
     }
 

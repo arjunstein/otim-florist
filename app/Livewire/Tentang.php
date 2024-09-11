759ff547957422b1b1bc162986e68791e7289ff3
Arjunstein <arjun.gnw09@gmail.com>
2024-09-11 13:21:34 +0700

    ✨ feat: add form input for subscription

--- app/Livewire/Tentang.php
+++ app/Livewire/Tentang.php
@@ -2,6 +2,7 @@
 
 namespace App\Livewire;
 
+use App\Models\Subscriber;
 use App\Models\Visitor;
 use Artesaos\SEOTools\Facades\JsonLd;
 use Artesaos\SEOTools\Facades\OpenGraph;
@@ -9,9 +10,49 @@ use Artesaos\SEOTools\Facades\SEOMeta;
 use Artesaos\SEOTools\Facades\TwitterCard;
 use Illuminate\Support\Facades\Storage;
 use Livewire\Component;
+use Livewire\Attributes\Validate;
 
 class Tentang extends Component
 {
+    public $fullname = '';
+    public $email = '';
+    public $messages = '';
+
+    public function subscriber()
+    {
+        // Lama sesi
+        $cooldownPeriod = 60 * 60 * 1;
+
+        if (session()->has('last_submitted')) {
+            $lastSubmitted = session('last_submitted');
+            $timeSinceLastSubmission = time() - $lastSubmitted;
+            $sisaSesi = $cooldownPeriod - $timeSinceLastSubmission;
+
+            if ($timeSinceLastSubmission < $cooldownPeriod) {
+                session()->flash('oops', "Tunggu {$sisaSesi} detik sebelum mengirim pesan lagi.");
+                return;
+            }
+        }
+
+        $validatedData = $this->validate([
+            'fullname' => 'required|min:3|max:30|string',
+            'email' => 'required|email|unique:subscribers,email|max:50',
+            'messages' => 'required|min:3|max:200|string',
+        ], [
+            'email.unique' => 'Email sudah terdaftar.',
+        ]);
+
+        $validatedData['fullname'] = strip_tags($validatedData['fullname']);
+        $validatedData['email'] = filter_var($validatedData['email'], FILTER_SANITIZE_EMAIL);
+        $validatedData['messages'] = strip_tags($validatedData['messages']);
+
+        Subscriber::create($validatedData);
+        session(['last_submitted' => time()]);
+
+        session()->flash('status', 'Pesan berhasil dikirim');
+        $this->reset(['fullname', 'email', 'messages']);
+    }
+
     public function mount()
     {
         SEOMeta::setTitle('Tentang kami');
